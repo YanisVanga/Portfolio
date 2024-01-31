7cb879f80a1f6f6e25d4d036bda41d7da5bbc857
YanisVanga <yanis.vang@gmail.com>
2024-01-31 18:22:39 +0100

    Curseur avec un flou

--- script.js
+++ script.js
@@ -7,17 +7,24 @@ class Script{
 
         this.color = document.querySelector(':root');
 
+        /*
+        var cursor = document.getElementById("cursor");
+        document.body.addEventListener("mousemove", function(e) {
+            cursor.style.left = e.clientX + "px",
+                cursor.style.top = e.clientY + "px";
+        });*/
+
     }
 
     changeMode(){
         if(this.color.style.getPropertyValue('--background') === '#111111'){
             this.btnDarkMode.classList.add("modeDark");
-            this.color.style.setProperty('--buttonBacground','#FFE500');
+            this.color.style.setProperty('--buttonBackground','#FFE500');
             this.color.style.setProperty('--background','#FFFFFF');
 
         }else{
             this.btnDarkMode.classList.remove("modeDark");
-            this.color.style.setProperty('--buttonBacground','#000000');
+            this.color.style.setProperty('--buttonBackground','#000000');
             this.color.style.setProperty('--background','#111111');
 
         }
